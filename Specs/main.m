cc1830098d335db56b98a676be3a904ed751c157
rbaumbach <rbaumbach.github@gmail.com>
2013-05-17 07:21:49 -0700

    Add Specs target for CEDAR testing.

---
+++ Specs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Specs
+//
+//  Created by Baumbud on 5/17/13.
+//  Copyright (c) 2013 RKB Designs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar-iOS/Cedar-iOS.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+    }
+}
