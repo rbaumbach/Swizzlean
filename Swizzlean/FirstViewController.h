a0e24209ef10819f1fbe6c69ad5c3a37480e9550
rbaumbach <rbaumbach.github@gmail.com>
2013-06-16 15:39:01 -0700

    Finish FirstViewController UI and tests.

--- Swizzlean/FirstViewController.h
+++ Swizzlean/FirstViewController.h
@@ -3,6 +3,19 @@
 
 @interface FirstViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *classLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *instanceMethodLabel;
+@property (weak, nonatomic) IBOutlet UILabel *instanceOutputLabel;
+@property (weak, nonatomic) IBOutlet UILabel *instanceMethodCalledOutputLabel;
+@property (weak, nonatomic) IBOutlet UILabel *instanceSwizzledOutputLabel;
+@property (weak, nonatomic) IBOutlet UILabel *instanceSwizzledCalledOutputLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *classMethodLabel;
+@property (weak, nonatomic) IBOutlet UILabel *classOutputLabel;
+@property (weak, nonatomic) IBOutlet UILabel *classMethodCalledOutputLabel;
+@property (weak, nonatomic) IBOutlet UILabel *classSwizzledOutputLabel;
+@property (weak, nonatomic) IBOutlet UILabel *classSwizzledCalledOutputLabel;
 
 @end
--- Swizzlean/FirstViewController.m
+++ Swizzlean/FirstViewController.m
@@ -1,6 +1,62 @@
 #import "FirstViewController.h"
+#import "Swizzlean.h"
 
+
+@interface FirstViewController ()
+
+@property(strong, nonatomic) Swizzlean *swizzlean;
+
+@end
 
 @implementation FirstViewController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.swizzlean = [[Swizzlean alloc] initWithClassToSwizzle:[NSString class]];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.classLabel.text = [NSString stringWithFormat:@"Class: %@", self.swizzlean.classToSwizzle];
+
+    [self instanceMethodTestingUI];
+    
+    self.classMethodLabel.text = @"Calling + [NSString pathWithComponents:]";
+}
+
+- (void)instanceMethodTestingUI
+{
+    self.instanceMethodLabel.text = @"Calling - [NSString intValue]";
+    
+    NSString *sevenHundredSeventySeven = @"777";
+    self.instanceMethodCalledOutputLabel.text = [NSString stringWithFormat:@"@\"777\" = %d", [sevenHundredSeventySeven intValue]];
+    
+    [self.swizzlean swizzleInstanceMethod:@selector(intValue)
+            withReplacementImplementation:^ (id _self) {
+                return 42;
+            }];
+    
+    self.instanceSwizzledCalledOutputLabel.text = [NSString stringWithFormat:@"@\"777\" = %d", [sevenHundredSeventySeven intValue]];
+    
+    [self.swizzlean resetSwizzledInstanceMethod];
+    
+    NSArray *pathArray = @[@"40", @"oz", @"beer", @"un-swizzled"];
+    self.classMethodCalledOutputLabel.text = [NSString pathWithComponents:pathArray];
+    
+    [self.swizzlean swizzleClassMethod:@selector(pathWithComponents:)
+         withReplacementImplementation:^(id _self, NSString *input) {
+             return @"24/oz/tallcan/beer/swizzled";
+         }];
+    
+    self.classSwizzledCalledOutputLabel.text = [NSString pathWithComponents:@[]];
+    
+    [self.swizzlean resetSwizzledClassMethod];
+}
 
 @end
