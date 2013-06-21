a9d06ea0149fc999276053a9238681d0ee4b5c7d
rbaumbach <rbaumbach.github@gmail.com>
2013-06-21 11:11:44 -0700

    Add dummy files.

---
+++ IntegrationTests/DummyTests.h
@@ -0,0 +1,13 @@
+//
+//  DummyTests.h
+//  Swizzlean
+//
+//  Created by Baumbud on 6/21/13.
+//  Copyright (c) 2013 RKB Designs. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DummyTests : SenTestCase
+
+@end
