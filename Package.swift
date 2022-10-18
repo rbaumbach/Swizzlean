27a9f5e30b27f5dc868d960a95340e066a56d54e
Ryan Baumbach <github@ryan.codes>
2022-10-17 21:07:42 -0600

    Use 'Specs' instead of 'Tests'

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .target(
             name: "Swizzlean"),
         .testTarget(
-            name: "SwizzleanTests",
+            name: "SwizzleanSpecs",
             dependencies: ["Swizzlean"])
     ]
 )
